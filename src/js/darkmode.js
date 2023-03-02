422fcda2ac41e9e372f322bfb9a9806b62696842
realcapobambino <35867381+realcapobambino@users.noreply.github.com>
2023-03-02 15:01:54 +0300

    added proper darkmode toggle switch

--- src/js/darkmode.js
+++ src/js/darkmode.js
@@ -1,58 +1,38 @@
+//////////////////////////////////////////////////////////////////
+//                                                              //
+//   Simple Dark Mode Switcher implementation by yours truly    //
+//                                                              //
+//////////////////////////////////////////////////////////////////
+
 // icons
-
-// const sunIcon = document.querySelector('.sun')
-// const moonIcon = document.querySelector('.moon')
-
 const lightSwitch = document.querySelector('.light-switch')
 
-
 // Theme Vars
-
 const userTheme = localStorage.getItem('theme')
 const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
-
-// Icon Toggling
-
-// const iconToggle = () => {
-//     sunIcon.classList.toggle('display-none');
-//     moonIcon.classList.toggle('display-none');
-
-// }
 
 // Initial Theme Check
 const themeCheck = () => {
     if (userTheme === "dark" || (!userTheme && systemTheme)) {
         document.documentElement.classList.add("dark")
-        // moonIcon.classList.add("display-none")
+
         return
     }
-    // sunIcon.classList.add("display-none")
 }
 // Manual Theme Switching
 const themeSwitch = () => {
     if (document.documentElement.classList.contains("dark")) {
         document.documentElement.classList.remove("dark")
         localStorage.setItem('theme', 'light')
-        // iconToggle()
         return
     }
     document.documentElement.classList.add("dark")
     localStorage.setItem('theme', 'dark')
-    // iconToggle()
 }
 
 // call theme switch on click
-// TODO: group the icons with a queryselectorall and loop through them using a forEach loop 
-// sunIcon.addEventListener('click', () => {
-//     themeSwitch()
-// })
-// moonIcon.addEventListener('click', () => {
-//     themeSwitch()
-// })
 lightSwitch.addEventListener('click', () => {
     themeSwitch()
 })
-
-
 // invoke theme check on initial page load
 themeCheck()
